016ca1756ede05b6e921585fb4e1daa7c8ea4778
LupusMaior <100087286+LupusMaior@users.noreply.github.com>
2022-05-24 21:25:09 -0500

    estudiante

---
+++ Estudiante.py
@@ -0,0 +1,5 @@
+class Estudiante:
+    
+    def _init_(self,NombreEstudiante:str,CodigoNumerico:str):
+          self.NombreEstudiante=NombreEstudiante
+          self.CodigoNumerico=CodigoNumerico
