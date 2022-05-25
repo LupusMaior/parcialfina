4de1747bdad9e2302e86a777ea0e60f456c8d282
LupusMaior <100087286+LupusMaior@users.noreply.github.com>
2022-05-24 21:19:31 -0500

    persona

---
+++ personapar.py
@@ -0,0 +1,16 @@
+from ast import Str
+
+
+class Persona:
+    def __init__(self, nombre:str, nif:int, fechaNac:int):
+
+        self.nombre = Nombre
+        self.nif=Nif
+        self.fechaNac =Fecha(int)
+
+class Jugador:
+    def __init__(self, numFed:int,):
+        self.numFed=numFed
+
+
+                                        
